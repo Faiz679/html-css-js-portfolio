981d504d438a18ee846ea27aa29bd89559a1bb4f
Faiz679 <daffa.makarim8@gmail.com>
2025-05-04 15:21:01 +0700

    04-mei-2025

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
 
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-};
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
 // CHANGING THEME //
 var head = document.head;
 var link = document.createElement("link");
-  
+
 link.type = "text/css";
 link.rel = "stylesheet";
 
 function themeDefault() {
-    link.href = "";
-};
+  link.href = "";
+}
 
 function themeBlueArchive() {
-    link.href = "blue-archive.css";
-};
+  link.href = "blue-archive.css";
+}
 
-function themeTerraria() {
-    link.href = "terraria.css";
-};
-  
 head.appendChild(link);
+
+function toggleTheme() {
+  const themeList = document.querySelectorAll(".theme-icon");
+  const themeText = document.querySelector(".theme-text");
+
+  themeList.forEach((n) => {
+    n.classList.toggle("open");
+  });
+  themeText.classList.toggle("theme-p");
+}
